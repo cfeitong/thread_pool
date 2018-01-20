7e4b47c5c411ee3732f081950fe2e97a4234cc61
cfeitong <cfeitong@gmail.com>
2018-01-20 16:57:47 +0800

    first working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,144 +1,132 @@
 #![feature(nll, box_syntax, iterator_step_by, fnbox)]
 
 use std::sync::{Condvar, Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::mpsc;
 use std::thread;
-use std::collections::VecDeque;
 use std::boxed::FnBox;
-use std::time::Duration;
 
-static MAX_JOBS: usize = 10;
+type Task = Box<FnBox() + Send + 'static>;
+
+struct SharedData {
+    empty: (Mutex<bool>, Condvar),
+    receiver: Mutex<mpsc::Receiver<Task>>,
+    n_threads: AtomicUsize,
+    n_active: AtomicUsize,
+}
 
 pub struct ThreadPool {
-    max_jobs: usize,
-    n_busy_thread: Mutex<usize>,
-    tasks: Arc<Mutex<VecDeque<Box<FnBox()+Send>>>>,
-    tasks_not_full: Arc<Condvar>,
-    tasks_not_empty: Arc<Condvar>,
-    not_busy: Arc<Condvar>,
-    senders: Vec<mpsc::Sender<bool>>,
+    sender: mpsc::Sender<Task>,
+    data: Arc<SharedData>,
 }
 
 impl ThreadPool {
     pub fn new(n_threads: usize) -> Self {
-        let tasks_not_full = Arc::new(Condvar::new());
-        let tasks_not_empty = Arc::new(Condvar::new());
-        let not_busy = Arc::new(Condvar::new());
-        let mut senders = Vec::new();
-        let tasks: Arc<Mutex<VecDeque<Box<FnBox()+Send>>>> = Arc::new(Mutex::new(VecDeque::new()));
+        let (tx, rx) = mpsc::channel::<Task>();
+        let data = Arc::new(
+            SharedData {
+                empty: (Mutex::new(false), Condvar::new()),
+                receiver: Mutex::new(rx),
+                n_threads: AtomicUsize::new(0),
+                n_active: AtomicUsize::new(0), 
+            }
+        );
+
         for _ in 0..n_threads {
-            let tasks_not_full = tasks_not_full.clone();
-            let tasks_not_empty = tasks_not_empty.clone();
-            let not_busy = not_busy.clone();
-            let (sx, tx) = mpsc::channel();
-            senders.push(sx);
-            let tasks = tasks.clone();
+            let data = data.clone();
             thread::spawn(move || {
-                'outer: loop {
-                    not_busy.notify_all();
-                    let task = {
-                        let tasks = tasks.lock().unwrap();
-                        let mut tasks = tasks_not_empty.wait(tasks).unwrap();
-                        while tasks.is_empty() {
-                            if tx.try_recv().is_err() {
-                                tasks = tasks_not_empty.wait(tasks).unwrap();
-                            }
-                            else {
-                                break 'outer;
-                            }
-                        }
-                        let task = tasks.pop_back();
-                        tasks_not_full.notify_all();
-                        task
+                data.n_threads.fetch_add(1, Ordering::SeqCst);
+
+                loop {
+                    let msg = {
+                        let rx = data.receiver.lock().expect(
+                            "working thread fails to lock receiver"
+                        );
+                        rx.recv()
                     };
-                    if let Some(task) = task { task(); }
+
+                    let task = match msg {
+                        Ok(task) => task,
+                        _ => break,
+                    };
+
+                    data.n_active.fetch_add(1, Ordering::SeqCst);
+
+                    task();
+
+                    data.n_active.fetch_sub(1, Ordering::SeqCst);
+
+                    if data.n_active.load(Ordering::SeqCst) == 0 {
+                        data.empty.1.notify_all();
+                    }
+
                 }
+
             });
         };
         ThreadPool {
-            max_jobs: MAX_JOBS,
-            n_busy_thread: Mutex::new(0),
-            tasks: tasks,
-            tasks_not_full: tasks_not_full,
-            tasks_not_empty: tasks_not_empty,
-            not_busy: not_busy,
-            senders: senders,
+            sender: tx,
+            data: data,
         }
     }
 
-    pub fn max_jobs(mut self, n_jobs: usize) -> Self {
-        self.max_jobs = n_jobs;
-        self
+    pub fn submit<F>(&self, func: F) 
+    where
+        F: FnOnce() + Send + 'static
+    {
+        let task: Task = box func;
+        self.sender.send(task).expect("fail to send task");
     }
 
-    pub fn submit(&mut self, func: Box<FnBox()+Send>) {
-        let mut task = self.tasks.lock().unwrap();
-        while !task.is_empty() && task.len() == self.max_jobs {
-            task = self.tasks_not_full.wait(task).unwrap();
+    pub fn join(&self) {
+        if self.data.n_active.load(Ordering::SeqCst) == 0 { return; }
+        let mut lock = self.data.empty.0.lock().unwrap();
+        while self.data.n_active.load(Ordering::SeqCst) != 0 {
+            lock = self.data.empty.1.wait(lock).unwrap();
         }
-        task.push_front(func);
-        let n_busy_thread = { *self.n_busy_thread.lock().unwrap() };
-        if n_busy_thread == self.max_jobs {
-            let _ = self.not_busy.wait(task).unwrap();
-        }
-        self.tasks_not_empty.notify_one();
     }
 }
 
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        loop {
-            let tasks = self.tasks.lock().unwrap();
-            if tasks.is_empty() {
-                for tx in &self.senders { tx.send(true).unwrap(); }
-                self.tasks_not_empty.notify_all();
-                break;
-            }
-            else {
-                self.tasks_not_empty.notify_all();
-                let _ = self.tasks_not_full.wait(tasks).unwrap();
-            }
-        }
-    }
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::time::Duration;
 
     #[test]
-    fn thread_pool_create_destroy() {
+    fn thread_pool_create_join() {
         let _pool = ThreadPool::new(100);
+        _pool.join();
     }
 
     #[test]
     fn thread_pool_submit() {
-        let mut pool = ThreadPool::new(2);
+        let pool = ThreadPool::new(2);
         let v: Arc<Mutex<Vec<i64>>> = Arc::new(Mutex::new(Vec::new()));
         let v1 = v.clone();
         let v2 = v.clone();
-        pool.submit(box move || { std::thread::sleep_ms(100); let mut v = v1.lock().unwrap(); v.push(1); });
-        pool.submit(box move || { let mut v = v2.lock().unwrap(); v.push(2); });
-        std::thread::sleep_ms(200);
+        pool.submit(move || { std::thread::sleep(Duration::from_millis(100)); let mut v = v1.lock().unwrap(); v.push(1); });
+        pool.submit(move || { let mut v = v2.lock().unwrap(); v.push(2); });
+        pool.join();
         let pv = v.lock().unwrap().clone();
         assert_eq!(pv, vec![2, 1])
     }
 
     #[test]
     fn thread_pool_schedule() {
-        let mut pool = ThreadPool::new(5);
         let v: Arc<Mutex<Vec<i64>>> = Arc::new(Mutex::new(Vec::new()));
+        let pool = ThreadPool::new(5);
         for i in 0..10 {
             let tv = v.clone();
-            pool.submit(box move || {
-                std::thread::sleep_ms((10-i)*40);
+            pool.submit(move || {
+                std::thread::sleep(Duration::from_millis((10-i) * 40));
                 let mut v = tv.lock().unwrap();
                 v.push(i as i64);
             });
         }
-        std::thread::sleep(Duration::from_secs(1));
+        pool.join();
         let mut pv = v.lock().unwrap().clone();
-        // pv.sort();
-        assert_eq!(pv, (0..10).collect::<Vec<i64>>());
+        pv.sort();
+        assert_eq!(pv, (0..10).collect::<Vec<_>>());
     }
 }
